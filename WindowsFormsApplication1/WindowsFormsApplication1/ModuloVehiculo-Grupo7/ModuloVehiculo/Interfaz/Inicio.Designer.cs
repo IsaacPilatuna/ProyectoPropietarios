5a5a86db759a47f492087094be89eb02179a6ef0
Chris_informatico <Chris_informatico@PC-SALON>
2018-08-12 16:33:29 -0500

    cambios e grupo 7

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloVehiculo-Grupo7/ModuloVehiculo/Interfaz/Inicio.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ModuloVehiculo.Interfaz
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.bntQuery = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Módulo De Vehiculo";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.bntQuery);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnADD);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(48, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(708, 339);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BIENVENIDO AL MÓDULO DE VEHICULO";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImage = global::ModuloVehiculo.Properties.Resources.if_common_logout_signout_exit_glyph_763291;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExit.Location = new System.Drawing.Point(537, 87);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnExit.Size = new System.Drawing.Size(151, 167);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // bntQuery
+            // 
+            this.bntQuery.BackgroundImage = global::ModuloVehiculo.Properties.Resources.if_67_171426;
+            this.bntQuery.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bntQuery.Location = new System.Drawing.Point(196, 87);
+            this.bntQuery.Name = "bntQuery";
+            this.bntQuery.Size = new System.Drawing.Size(151, 167);
+            this.bntQuery.TabIndex = 2;
+            this.bntQuery.UseVisualStyleBackColor = true;
+            this.bntQuery.Click += new System.EventHandler(this.bntQuery_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImage = global::ModuloVehiculo.Properties.Resources.if_cross_24_103181;
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDelete.Location = new System.Drawing.Point(377, 87);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnDelete.Size = new System.Drawing.Size(151, 167);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnADD
+            // 
+            this.btnADD.BackgroundImage = global::ModuloVehiculo.Properties.Resources.if_199_CircledPlus_183316;
+            this.btnADD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnADD.Location = new System.Drawing.Point(17, 87);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(155, 167);
+            this.btnADD.TabIndex = 0;
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button bntQuery;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloVehiculo-Grupo7/ModuloVehiculo/Interfaz/Inicio.cs
@@ -0,0 +1,56 @@
+﻿using ModuloVehiculo.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModuloVehiculo.Interfaz
+{
+    public partial class Inicio : Form
+    {
+        
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            VehiculoFormulario vf = new VehiculoFormulario(1);
+            vf.Show();
+            
+        }
+
+        private void bntQuery_Click(object sender, EventArgs e)
+        {
+            VehiculoFormulario vf = new VehiculoFormulario(2);
+            vf.Show();
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            VehiculosDelte vd = new VehiculosDelte ();
+            vd.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            
+            toolTip1.SetToolTip(btnADD, "Añadir: Permite agregar un nuevo vehiculo");
+            toolTip1.SetToolTip(btnDelete, "Eliminar: Permite eliminar un vehiculo");
+            toolTip1.SetToolTip(btnExit, "Salir: Permite salir del módulo");
+            toolTip1.SetToolTip(bntQuery, "Consultar: Permite consultar los vehiculos existente y modificar su información");
+        }
+    }
+}
