8e339469ba865d8fbad5a7527bc7f3b470900d7b
VICTOR FERNANDO CARRASCO MENA <victor.carrasco@epn.edu.ec>
2018-08-12 21:23:00 -0500

    inserción del modulo rutas

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloRutas/Formularios/DatosKilometraje.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication1.ModuloRutas.Formularios
+{
+    partial class DatosKilometraje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataKilometraje = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataKilometraje)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HISTORIAL KILOMETRAJE";
+            // 
+            // dataKilometraje
+            // 
+            this.dataKilometraje.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataKilometraje.Location = new System.Drawing.Point(103, 138);
+            this.dataKilometraje.Name = "dataKilometraje";
+            this.dataKilometraje.RowTemplate.Height = 24;
+            this.dataKilometraje.Size = new System.Drawing.Size(638, 277);
+            this.dataKilometraje.TabIndex = 1;
+            this.dataKilometraje.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DatosKilometraje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataKilometraje);
+            this.Controls.Add(this.label1);
+            this.Name = "DatosKilometraje";
+            this.Text = "DatosKilometraje";
+            this.Load += new System.EventHandler(this.DatosKilometraje_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataKilometraje)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataKilometraje;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloRutas/Formularios/Incidencia.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.ModuloRutas.Formularios
+{
+    public partial class Incidencia : Form
+    {
+        Clases.Conexion con = new Clases.Conexion();
+        int id = 0;
+        string placa;
+        string timeP;
+        string hora;
+        string descripcion;
+
+        public int getid() { return this.id; }
+        public void setid(int id) { this.id = id; }
+        public string getplaca() { return this.placa; }
+        public void setplaca(string placa) { this.placa = placa; }
+        public string gettimeP() { return this.timeP; }
+        public void settimeP(string timeP) { this.timeP = timeP; }
+        public string gethora() { return this.hora; }
+        public void sethora(string hora) { this.hora = hora; }
+        public string getdescripcion() { return this.descripcion; }
+        public void setdescripcion(string descripcion) { this.descripcion = descripcion; }
+        public Incidencia()
+        {
+            InitializeComponent();
+        }
+
+        private void incidenciaIDTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string n = incidenciaID.Text;
+            id = int.Parse(n);
+        }
+
+        private void Incidencia_Load(object sender, EventArgs e)
+        {
+            con.llenarItems(PLACAVEHICULO);
+        }
+
+        private void PLACAVEHICULO_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //placa = Convert.ToString(PLACAVEHICULO.SelectedValue);
+            //placa = Convert.ToString(PLACAVEHICULO.SelectedText);
+        }
+
+        private void horaIncidencia_TextChanged(object sender, EventArgs e)
+        {
+            hora = horaIncidencia.Text;
+        }
+
+        private void fechaIncidencia_ValueChanged(object sender, EventArgs e)
+        {
+            timeP = fechaIncidencia.Text;
+        }
+
+        private void descripcionD_TextChanged(object sender, EventArgs e)
+        {
+            descripcion = descripcionD.Text;
+        }
+
+        private void GUARDAR_Click(object sender, EventArgs e)
+        {
+            placa = Convert.ToString(this.PLACAVEHICULO.Text);
+            try{
+                con.query("insert into incidencia values('" + id + "','" + hora + "','" + timeP + "','" + descripcion + "','" + placa + "');");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Campos no válidos" + ex.ToString());
+            }
+            
+            MessageBox.Show("Registro ingresado con exito");
+        }
+    }
+}
