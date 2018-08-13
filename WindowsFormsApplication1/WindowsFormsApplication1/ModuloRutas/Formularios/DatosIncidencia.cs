ad86b367332d484acabdbcb6b247d55b35a21543
VICTOR FERNANDO CARRASCO MENA <victor.carrasco@epn.edu.ec>
2018-08-12 19:13:28 -0500

    inserción de 2 clases del modulo rutas

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloRutas/Formularios/DatosIncidencia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.ModuloRutas.Formularios
+{
+    
+    public partial class DatosIncidencia : Form
+    {
+        Clases.Conexion con = new Clases.Conexion();
+        string placa;
+        public DatosIncidencia()
+        {
+            InitializeComponent();
+        }
+
+        private void dataIncidecia_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //con.CargarDatos(placa,dataIncidecia);
+        }
+
+        private void CBPlacas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void DatosIncidencia_Load(object sender, EventArgs e)
+        {
+       
+            //con.CargarDatos(placa, dataIncidecia);
+            con.CargarDatosIncidencia(dataIncidecia);
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloRutas/Formularios/DatosKilometraje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.ModuloRutas.Formularios
+{
+    public partial class DatosKilometraje : Form
+    {
+        Clases.Conexion con = new Clases.Conexion();
+        public DatosKilometraje()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DatosKilometraje_Load(object sender, EventArgs e)
+        {
+            con.CargarDatosKilometraje(dataKilometraje);
+        }
+    }
+}
